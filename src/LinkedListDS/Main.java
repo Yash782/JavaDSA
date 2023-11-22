0473321942e92248e9b777597f157c36248bcae8
Yash Wadgaonkar <89408284+Yash782@users.noreply.github.com>
2023-11-22 22:40:39 +0530

    This Project contains data structure which I learn from now and some problems related to that

--- src/LinkedListDS/Main.java
+++ src/LinkedListDS/Main.java
@@ -1,4 +1,19 @@
 package LinkedListDS;
 
 public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(1);
+        list.insertFirst(21);
+        list.insertFirst(123);
+        list.insertFirst(15);
+        list.insertFirst(18);
+        list.insertFirst(45);
+        list.insertFirst(78);
+        list.insertFirst(89);
+        list.insertLast(100);
+
+        list.display();
+
+    }
 }
